be9cc22a0b62a271a1ac633039c71a0e491d69d6
killex8569 <alexandreanette@gmail.com>
2025-09-28 15:04:11 +0200

    test

--- projet 1 test/src/main.c
+++ projet 1 test/src/main.c
@@ -6,16 +6,17 @@
 
 int main(void) // Initialisation de la fonction main, le void entre parenthèse est un paramètre, le int permet de dire que le retour de la fonction main est un int
 {
+    static int User = 18; // Ici le static permet d'initialiser la variable une seul fois lors de l'utilisation de la fonction
+    int UserAge = 45; // Affecte la valeur 45 à la variable UserAge, donc UserAge vaut 45
+    UserAge = 20;
+
     printf("Hello World!\n"); // Printf qui permet de print le texte entre "" et \n qui permet de retourner à la ligne
     return 0; // Retourne 0, permet de terminer la fonction
 }
 
-// Partie 2 : Variables
+const int USER_AGE = 0; // Ici on peut mettre des "variable Global", mais c'est pas conseiller, sauf pour les constante, pour cela on met "const" juste devant la variable
+const float PI = 3.14; // On met généralement les nom en majuscule des variables qui sont constantes
 
-// On déclare ici une nouvelle fonction pour nos variables
 
-int variables(void)
-{
-    printf("test du languages C \n");
-    return 0; // Pas de problème si c'est 0
-}
+
+
