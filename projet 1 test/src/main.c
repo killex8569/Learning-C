cc2ee70844f227e5c93ce97e579efe1dca16ef04
killex8569 <alexandreanette@gmail.com>
2025-09-28 14:30:18 +0200

    commit

---
+++ projet 1 test/src/main.c
@@ -0,0 +1,21 @@
+// Base du projet C
+
+// Partie 1 :  Les bases du C
+
+#include <stdio.h> // Inclusion du fichier stdio.h, les points H permettent de centraliser des fonctions
+
+int main(void) // Initialisation de la fonction main, le void entre parenthèse est un paramètre, le int permet de dire que le retour de la fonction main est un int
+{
+    printf("Hello World!\n"); // Printf qui permet de print le texte entre "" et \n qui permet de retourner à la ligne
+    return 0; // Retourne 0, permet de terminer la fonction
+}
+
+// Partie 2 : Variables
+
+// On déclare ici une nouvelle fonction pour nos variables
+
+int variables(void)
+{
+    printf("test du languages C \n");
+    return 0; // Pas de problème si c'est 0
+}
